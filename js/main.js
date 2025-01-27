d983e99e3300dfde80bbf9af0660464debb73169
noorulhuq <s.noorulhuq@gmail.com>
2025-01-27 12:47:57 +0530

    updated file with carousel

--- js/main.js
+++ js/main.js
@@ -115,3 +115,25 @@
     
 })(jQuery);
 
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+      },
+    });
+  });
+  
